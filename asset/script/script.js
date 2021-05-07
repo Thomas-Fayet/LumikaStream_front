a4af176339ce2b8e291eabb1647b3ae5cfa49630
Thomas-Fayet <t.fayet@hotmail.fr>
2021-05-07 13:34:14 +0200

    commit

--- asset/script/script.js
+++ asset/script/script.js
@@ -8,6 +8,8 @@ let navBurger = document.getElementById("nav-burger");
 let body = document.body;
 let actualisation = window.sessionStorage;
 
+var inputField = document.querySelector(".message_form__input");
+
 // --------------------------------------------- GESTION DES BOUCLES / FONCTIONS / METHODES ---------------------------------------------
 
 menuIcon.addEventListener('click', () => {
@@ -41,11 +43,11 @@ if (actualisation.getItem("is_reloaded")) {
 
 let btn = document.getElementById('profil-picture-btn');
 let containerPicture = document.getElementById('profil-picture-container');
-let file = document.querySelector('input[type=file]').files;
+//let file = document.querySelector('input[type=file]').files;
 let preview = document.getElementById('preview-image');
 // let reader = new FileReader();
-console.log(file);
-console.log(file.type);
+//console.log(file);
+//console.log(file.type);
 // console.log(reader);
 
 let fileTypes = [
@@ -54,7 +56,7 @@ let fileTypes = [
     'image/jpg' 
 ]
 
-btn.addEventListener("change", previewFile);
+//btn.addEventListener("change", previewFile);
 
 
 // --------------------------------------------- GESTION DES BOUCLES / FONCTIONS / METHODES ---------------------------------------------
@@ -75,7 +77,7 @@ function previewFile() {
 }
 
 
-console.log(window.URL.createObjectURL(file[0]))
+//console.log(window.URL.createObjectURL(file[0]))
 
 function validFileType(file) {
     for (let i = 0; i < fileTypes.length; i++) {
@@ -96,3 +98,114 @@ function validFileType(file) {
 // reader.addEventListener("onload", () => {
 //     preview.src = reader.result;
 // })
+
+// ------------------------------------------------ GESTION DU STREAM ------------------------------/
+
+var stream = document.getElementById('stream')
+stream.addEventListener('click', () => {
+    console.log('salut');
+    fetch("http://localhost:3000")
+        .then(response => {
+            if (flvjs.isSupported()) {
+                var videoElement = document.getElementById('videoElement');
+                var flvPlayer = flvjs.createPlayer({
+                    type: 'flv',
+                    url: 'http://localhost:8000/live/STREAM_NAME.flv'
+                });
+                flvPlayer.attachMediaElement(videoElement);
+                flvPlayer.load();
+                flvPlayer.play();
+            }
+        })
+})
+
+// ---------------------------------------------- GESTION DU CHAT --------------------------------------
+
+inputField.addEventListener('focusin', () => {
+    var socket = io('http://localhost:3000');
+    var messageForm = document.querySelector(".message_form");
+    var messageBox = document.querySelector(".messages__history");
+    var inboxPeople = document.querySelector(".inbox__people");
+    var fallback = document.querySelector(".fallback");
+
+
+    let userName = "";
+
+    var newUserConnected = (user) => {
+        userName = user || `User${Math.floor(Math.random() * 1000000)}`;
+        socket.emit("new user", userName);
+        addToUsersBox(userName);
+    };
+
+    var addToUsersBox = (userName) => {
+        if (!!document.querySelector(`.${userName}-userlist`)) {
+            return;
+        }
+
+        const userBox = `
+        <div class="chat_ib ${userName}-userlist">
+        <h5>${userName}</h5>
+        </div>
+    `;
+        inboxPeople.innerHTML += userBox;
+    };
+
+    // new user is created so we generate nickname and emit event
+    newUserConnected();
+
+    socket.on("new user", function (data) {
+        data.map((user) => addToUsersBox(user));
+    });
+
+    socket.on("user disconnected", function (userName) {
+        document.querySelector(`.${userName}-userlist`).remove();
+    });
+
+    //new message
+
+    var addNewMessage = ({ user, message }) => {
+        var time = new Date();
+        var formattedTime = time.toLocaleString("en-US", { hour: "numeric", minute: "numeric" });
+
+        var receivedMsg = `
+        <div class="received__message">
+            <p><span class="user-chat">${user} </span> : ${message}</p>
+        </div>`;
+
+        var myMsg = `
+        <div class="sent__message">
+            <p><span class="user-chat">${user} </span> : ${message}</p>
+        </div>`;
+
+        messageBox.innerHTML += user === userName ? myMsg : receivedMsg;
+    };
+
+    // new user is created so we generate nickname and emit event
+    newUserConnected();
+
+    messageForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        if (!inputField.value) {
+        return;
+        }
+    
+        socket.emit("chat message", {
+        message: inputField.value,
+        nick: userName,
+        });
+    
+        inputField.value = "";
+    });
+
+    socket.on("new user", function (data) {
+        data.map((user) => addToUsersBox(user));
+    });
+    
+    socket.on("user disconnected", function (userName) {
+        document.querySelector(`.${userName}-userlist`).remove();
+    });
+    
+    socket.on("chat message", function (data) {
+        addNewMessage({ user: data.nick, message: data.message });
+    });
+});
